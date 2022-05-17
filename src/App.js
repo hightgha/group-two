ad29152e17c7292354227fb75a73e81d93e3b824
high.tgha <94756849+hightgha@users.noreply.github.com>
2022-05-17 20:05:46 +0400

    navbar + routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
     <div className='App'>
-      <div>Hi</div>
+      <Navbar />
+
+      <Routes>
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
     </div>
   );
 }
