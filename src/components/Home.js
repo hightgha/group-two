cf4dbbcc593a93f2469b9339e4ce53a2e8e47146
high.tgha <94756849+hightgha@users.noreply.github.com>
2022-05-18 19:09:33 +0400

    CSS => JSS, added minimal version hompage and Hotel.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,38 @@
-export default function Home() {
-  return <h1>Home</h1>;
+import { makeStyles, Typography } from '@material-ui/core';
+import Hotel from './Hotel';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    margin: 'auto',
+    display: 'flex',
+    justifyContent: 'center',
+    width: 700,
+  },
+  blockText: {
+    display: 'block',
+    margin: 8,
+    textDecoration: 'underline',
+    textTransform: 'uppercase',
+  },
+}));
+
+export default function Home(props) {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <div className={classes.container}>
+        <Hotel />
+        <div>
+          <Typography className={classes.blockText} variant='body1'>
+            Any information about hotel number
+          </Typography>
+          <Typography className={classes.blockText} variant='caption'>
+            Description about hotel number
+          </Typography>
+        </div>
+      </div>
+    </div>
+  );
 }
