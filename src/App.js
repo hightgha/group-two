d644a50f3b71647106596ad73ca1200098397048
arevikmosinyan <aarreevviikk1@gmail.com>
2022-05-18 20:58:56 +0400

    main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import About from './components/About';
-import Auth from './components/Auth';
-import Home from './components/Home';
-import Navbar from './components/Navbar';
-import Profile from './components/Profile';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./components/About";
+import Auth from "./components/Auth";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 export default function App() {
   return (
@@ -12,9 +12,9 @@ export default function App() {
       <Navbar />
 
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/profile' element={<Profile />} />
+        <Route path='/' element={<Home />} />+
+        <Route path='/about' element={<About />} />+
+        <Route path='/profile' element={<Profile />} />+
         <Route path='/auth' element={<Auth />} />
       </Routes>
     </div>
